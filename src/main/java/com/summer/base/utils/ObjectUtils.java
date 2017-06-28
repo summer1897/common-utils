bd5b8765e06bbce425e7667dbca1a4c5f8270ca3
summer1897 <406507191@qq.com>
2017-06-28 22:57:14 +0800

    @feature 添加时间日期工具方法

--- src/main/java/com/summer/base/utils/ObjectUtils.java
+++ src/main/java/com/summer/base/utils/ObjectUtils.java
@@ -1,6 +1,8 @@
 package com.summer.base.utils;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -11,22 +13,88 @@ import java.util.List;
  */
 public class ObjectUtils {
 
-    public static boolean isNull(Object obj){
+    /**
+     * 默认时间格式
+     */
+    public static final String DEFAULT_TIME_FORMAT = "hh:mm:ss";
+    /**
+     * 默认日期格式
+     */
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+    /**
+     * 默认日期（含时间）格式
+     */
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
+
+    public static boolean isNull(Object obj) {
         return null == obj;
     }
 
-    public static boolean isNotNull(Object obj){
+    public static boolean isNotNull(Object obj) {
         return !isNull(obj);
     }
 
     /**
      * 判断集合是否为空
-     * @param list
-     * @return
+     *
+     * @param collection
+     * @return true为空，false不为空
      */
-    public static boolean isEmptyList(Collection<?> list) {
-        if(null == list || list.size() <= 0)
+    public static boolean isEmptyList(Collection<?> collection) {
+        if (null == collection || collection.size() <= 0)
             return true;
         return false;
+    }
+
+    /**
+     * 判断集合是否为空
+     *
+     * @param collection
+     * @return false为空，true不为空
+     */
+    public static boolean isNotEmptyList(Collection<?> collection) {
+        return !ObjectUtils.isEmptyList(collection);
+    }
+
+    private static SimpleDateFormat getFormat(String format) {
+        return new SimpleDateFormat(format);
+    }
+
+    /**
+     * 获取当前时间（不包含日期)
+     *
+     * @param format 时间显示格式
+     * @return 时间字符串，如13:34:38
+     */
+    public static String getCurrenTime(String format) {
+        if (StringUtils.isEmpty(format)) {
+            format = DEFAULT_TIME_FORMAT;
+        }
+
+        return ObjectUtils.getFormat(format).format(new Date());
+    }
+
+    /**
+     * 获取当前日期（不包含时间)
+     * @param format 日期显示格式
+     * @return 日期字符串，如2017-06-25
+     */
+    public static String getCurrenDate(String format) {
+        if (StringUtils.isEmpty(format)) {
+            format = DEFAULT_DATE_FORMAT;
+        }
+        return ObjectUtils.getFormat(format).format(new Date());
+    }
+
+    /**
+     * 获取当前日期（含时间)
+     * @param format 日期显示格式
+     * @return 日期字符串，如2017-06-25 13:34:38
+     */
+    public static String getCurrenDateAndTime(String format) {
+        if (StringUtils.isEmpty(format)) {
+            format = DEFAULT_DATE_TIME_FORMAT;
+        }
+        return ObjectUtils.getFormat(format).format(new Date());
     }
 }
